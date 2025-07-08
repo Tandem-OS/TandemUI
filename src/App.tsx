ec81b76eb9a989a279afc9225d79b877bb952add
Syed Moazzam <sma.hyetelx@gmail.com>
2025-07-08 14:48:45 +0500

    Removed erros

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./lib/providers/AuthProvider"; // Adjust the import path as needed
 import Layout from "./Layout";
+import SessionManager from "./components/auth/form/SessionManager";
 
 const App = () => {
   return (
     <BrowserRouter> 
       <AuthProvider>
+        <SessionManager />
         <Layout />
       </AuthProvider>
     </BrowserRouter>
