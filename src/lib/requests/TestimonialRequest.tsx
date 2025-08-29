06a53beefa5fd5b18a2fc390d924c0f2facd7344
Syed Moazzam <sma.hyetelx@gmail.com>
2025-08-29 21:00:30 +0500

    testimonial integration complete

---
+++ src/lib/requests/TestimonialRequest.tsx
@@ -0,0 +1,63 @@
+import api from '@/lib/requests/Axios'
+import { store } from "@/store";
+
+interface DesignerTestimonial {
+  rating: number;
+  standout: string;
+  recommend: string;
+}
+
+export const designerTestimonialSubmission = async (values: DesignerTestimonial) => {
+  const clientEmail = store.getState().auth.user.email!;
+  const designerEmail = store.getState().auth.user.designerEmail;
+  const projectId = store.getState().project.projectId;
+
+  const payload = {
+    ...values,
+    client_email: clientEmail,
+    designer_email: designerEmail,
+    project_id: projectId
+  };
+
+  return await api.post("/testimonial/submit", payload);
+}
+
+
+export const paltformTestimonialSubmission = async (values: DesignerTestimonial) => {
+  const clientEmail = store.getState().auth.user.email!;
+  const projectId = store.getState().project.projectId;
+
+  const payload = {
+    ...values,
+    client_email: clientEmail,
+    project_id: projectId
+  };
+
+  return await api.post("/testimonial/platform/submit", payload);
+}
+
+export const retriveDesignerTestimonial = async () => {
+  const clientEmail = store.getState().auth.user.email!;
+  const designerEmail = store.getState().auth.user.designerEmail;
+  const projectId = store.getState().project.projectId;
+
+  const payload = {
+    client_email: clientEmail,
+    designer_email: designerEmail,
+    project_id: projectId
+  };
+
+  return await api.post("/testimonial/designer", payload);
+}
+
+export const retrivePlatformTestimonial = async () => {
+  const clientEmail = store.getState().auth.user.email!;
+  const projectId = store.getState().project.projectId;
+
+  const payload = {
+    client_email: clientEmail,
+    project_id: projectId
+  };
+
+  return await api.post("/testimonial/platform", payload);
+}
